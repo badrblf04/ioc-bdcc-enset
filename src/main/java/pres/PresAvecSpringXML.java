8b494600bca89de6d8251782d68983e5f64e645b
Badr <badham048@gmail.com>
2025-04-15 19:36:59 +0200

    V1

--- src/main/java/pres/PresAvecSpringXML.java
+++ src/main/java/pres/PresAvecSpringXML.java
@@ -4,7 +4,7 @@ import metier.IMetier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class PresAvecSoringXML {
+public class PresAvecSpringXML {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         IMetier metier = (IMetier) context.getBean(IMetier.class);
